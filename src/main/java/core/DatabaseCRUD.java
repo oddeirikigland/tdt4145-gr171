d9028a8b3221e197b03f4ba3421509c68be5153b
Vegar Andreas Bergum <vegarab@stud.ntnu.no>
2018-03-06 10:29:03 +0100

    implements DatabaseCRUD interface

--- src/main/java/core/DatabaseCRUD.java
+++ src/main/java/core/DatabaseCRUD.java
@@ -1,6 +1,31 @@
 package core;
 
 public interface DatabaseCRUD {
-    
+
+    /**
+     * Persisted object to database
+     * @param object    object to persist
+     * @return id of persisted object
+     */
+    int create(Object object);
+
+    /**
+     * Retrieves persisted object from database
+     * @param id    of object to retrieve
+     * @return object retrieved
+     */
+    Object retrieve(int id);
+
+    /**
+     * Updates persisted object based on new object data
+     * @param object to be persisted
+     */
+    void update(Object object);
+
+    /**
+     * Deletes persisted object from database
+     * @param id of object to delete
+     */
+    void delete(int id);
 
 }
