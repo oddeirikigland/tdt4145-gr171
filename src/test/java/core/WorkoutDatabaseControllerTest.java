66af1a1e7e25af715b4b6bddec92f1b9838c3fd4
Vegar Andreas Bergum <vegarab@stud.ntnu.no>
2018-03-06 13:30:53 +0100

    Finished WorkoutDatbaseControllerTest

--- src/test/java/core/WorkoutDatabaseControllerTest.java
+++ src/test/java/core/WorkoutDatabaseControllerTest.java
@@ -1,9 +1,9 @@
 package core;
 
-import static org.junit.Assert.*;
-
+import java.io.IOException;
 import java.sql.Date;
 
+import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
@@ -16,8 +16,11 @@ public class WorkoutDatabaseControllerTest {
 	
 	@BeforeClass
 	public static void beforeClass() {
-		CreateDatabase.main(null);
-
+		try {
+			DatabaseHandler.database(false);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	@Before
@@ -33,5 +36,46 @@ public class WorkoutDatabaseControllerTest {
 		
 		Assert.assertNotEquals(-1, w1.getWorkoutID());
 	}
-
+	
+	@Test
+	public void testRetrieveExpectPersistedObject() {
+		w1 = new Workout(wdc.create(w1), w1.getTimestamp(), w1.getDuration(), w1.getForm(), w1.getPerformance(), w1.getNote());
+		w2 = wdc.retrieve(w1.getWorkoutID());
+		
+		Assert.assertEquals(w2.getWorkoutID(), w1.getWorkoutID());
+		Assert.assertEquals(w2.getTimestamp(), w1.getTimestamp());
+		Assert.assertEquals(w2.getDuration(), w1.getDuration());
+		Assert.assertEquals(w2.getForm(), w1.getForm());
+		Assert.assertEquals(w2.getPerformance(), w1.getPerformance());
+		Assert.assertEquals(w2.getNote(), w1.getNote());
+	}
+	
+	@Test
+	public void testUpdateExpectPersistedObjet() {
+		w1 = new Workout(wdc.create(w1), w1.getTimestamp(), w1.getDuration(), w1.getForm(), w1.getPerformance(), w1.getNote());
+		w2 = new Workout(w1.getWorkoutID(), w2.getTimestamp(), w2.getDuration(), w2.getForm(), w2.getPerformance(), w2.getNote());
+		wdc.update(w2);
+		w2 = wdc.retrieve(w1.getWorkoutID());
+		
+		Assert.assertEquals(w1.getWorkoutID(), w2.getWorkoutID());
+		Assert.assertNotEquals(w2.getNote(), w1.getNote());
+	}
+	
+	@Test
+	public void testDeleteExpectNull() {
+		w1 = new Workout(wdc.create(w1), w1.getTimestamp(), w1.getDuration(), w1.getForm(), w1.getPerformance(), w1.getNote());
+		wdc.delete(w1.getWorkoutID());
+		w2 = wdc.retrieve(w1.getWorkoutID());
+		
+		Assert.assertEquals(null, w2);
+	}
+	
+	@AfterClass
+	public static void wipeDatabase() {
+		try {
+			DatabaseHandler.database(true);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
