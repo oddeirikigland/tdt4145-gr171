95d4484fbf5f3524490b62e3c08467f88bb9bc1e
Vegar Andreas Bergum <vegarab@stud.ntnu.no>
2018-03-06 13:30:17 +0100

    DatabaseHandler with SQL-queries

---
+++ src/main/resources/wipe.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS 'machine_exercise';
+DROP TABLE IF EXISTS 'free_exericse';
+DROP TABLE IF EXISTS 'exercise';
+DROP TABLE IF EXISTS 'exercise_done';
+DROP TABLE IF EXISTS 'includes';
+DROP TABLE IF EXISTS 'workout';
+DROP TABLE IF EXISTS 'exercise_group';
+DROP TABLE IF EXISTS 'machine';
+
